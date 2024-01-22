b8ef7e829c7fc51c4da970638f192e025144f1d5
Deep <75555496+deepatavy@users.noreply.github.com>
2024-01-22 21:37:08 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/invideo/calculator_plus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.invideo.calculator_plus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Calculator Sample',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatelessWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator Plus'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
