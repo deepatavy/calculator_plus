32c4a1222fec4d8f3042c533c032e68a2d18e9a7
Deep <75555496+deepatavy@users.noreply.github.com>
2024-01-23 20:45:43 +0530

    Regexp updated

--- lib/utils/regexp.dart
+++ lib/utils/regexp.dart
@@ -1,5 +1,1 @@
-RegExp invalidCalculatorRegex = RegExp(
-  r'^\s*'
-  r'[^0-9+\-*/().\s]' // Match any character that is not a number, operator, parentheses, dot, or whitespace
-  r'\s*$',
-);
+RegExp validRegExp = RegExp(r'[0-9+\-*/().\s]');
